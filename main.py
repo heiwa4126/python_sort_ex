ff042c054ee4f12b720da86a1f24a9ab6205e17c
heiwa4126 <heiwa4126@gmail.com>
2022-09-13 11:36:47 +0900

    add merge sort

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def gen_list() -> list[int]:
 
 
 def selection_sort(a: list[int]) -> list[int]:
+    if len(a) <= 1:
+        return a
     for i in range(0, len(a)):
         min_idx = i
         for j in range(i, len(a)):
@@ -23,10 +25,34 @@ def selection_sort(a: list[int]) -> list[int]:
 
 
 def bubble_sort(a: list[int]) -> list[int]:
+    if len(a) <= 1:
+        return a
     for i in range(0, len(a) - 1):
         for j in range(1, len(a) - i):
             if a[j] < a[j - 1]:
                 a[j], a[j - 1] = a[j - 1], a[j]
+    return a
+
+
+def merge_sort(a: list[int]) -> list[int]:
+    if len(a) <= 1:
+        return a
+    t = int(len(a) / 2)
+    l = merge_sort(a[:t])
+    r = merge_sort(a[t:])
+    a = list()
+
+    while True:
+        if len(l) == 0 and len(r) > 0:
+            a.extend(r)
+            break
+        elif len(l) > 0 and len(r) == 0:
+            a.extend(l)
+            break
+        if l[0] < r[0]:
+            a.append(l.pop(0))
+        else:
+            a.append(r.pop(0))
     return a
 
 
@@ -42,6 +68,7 @@ def main():
     """main"""
     run_a_sort("selection sort", selection_sort)
     run_a_sort("bubble sort", bubble_sort)
+    run_a_sort("merge sort", merge_sort)
 
 
 if __name__ == "__main__":
