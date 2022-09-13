cbab764e2ed12920b916876ffe614799ad1225e0
heiwa4126 <heiwa4126@gmail.com>
2022-09-13 10:34:51 +0900

    add bubble sort

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from https://cs50.jp/x/2021/week3/notes/
 from typing import Callable
 
 
+def gen_list() -> list[int]:
+    return [6, 3, 8, 5, 2, 7, 4, 1]
+
+
 def selection_sort(a: list[int]) -> list[int]:
     for i in range(0, len(a)):
         min_idx = i
@@ -18,8 +22,12 @@ def selection_sort(a: list[int]) -> list[int]:
     return a
 
 
-def gen_list() -> list[int]:
-    return [6, 3, 8, 5, 2, 7, 4, 1]
+def bubble_sort(a: list[int]) -> list[int]:
+    for i in range(0, len(a) - 1):
+        for j in range(1, len(a) - i):
+            if a[j] < a[j - 1]:
+                a[j], a[j - 1] = a[j - 1], a[j]
+    return a
 
 
 def run_a_sort(name: str, f: Callable):
@@ -33,6 +41,7 @@ def run_a_sort(name: str, f: Callable):
 def main():
     """main"""
     run_a_sort("selection sort", selection_sort)
+    run_a_sort("bubble sort", bubble_sort)
 
 
 if __name__ == "__main__":
